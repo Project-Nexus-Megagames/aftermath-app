44fbde821360d542187dc18daa55fc20b656210c
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-10-20 00:06:41 +0200

    putting a flag on the map

--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -1,47 +1,46 @@
-import React, { useRef, useCallback, useEffect } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import { mapstyle } from "../config/mapStyles";
-import { Box, Button, Spinner, Text } from "@chakra-ui/react";
+import React, { useRef, useCallback, useEffect } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { mapstyle } from '../config/mapStyles';
+import { Box, Button, Spinner, Text } from '@chakra-ui/react';
+import { MarkerF } from '@react-google-maps/api';
 
-const MAPKEY = process.env.REACT_APP_MAPKEY || "";
-const libraries = ["places"];
+const MAPKEY = process.env.REACT_APP_MAPKEY || '';
+const libraries = ['places'];
 const mapContainerStyle = {
-  width: "100%",
-  height: "100%",
+	width: '100%',
+	height: '100%'
 };
 
 export const AftermathMap = () => {
-  const mapOptions = {
-    styles: mapstyle,
-    disableDefaultUI: true,
-    zoomControl: true,
-    mapTypeControl: true,
-    // restriction: {
-    //   latLngBounds: {
-    //     north: 50,
-    //     south: -50,
-    //     east: 1600,
-    //     west: 1000,
-    //   },
-    // },
-    minZoom: 3,
-    maxZoom: 9,
-    //mapTypeId: 'terrain'
-  };
-  const center = {
-    lat: -3.745,
-    lng: -38.523,
-  };
+	const mapOptions = {
+		styles: mapstyle,
+		disableDefaultUI: true,
+		zoomControl: true,
+		mapTypeControl: true,
+		// restriction: {
+		//   latLngBounds: {
+		//     north: 50,
+		//     south: -50,
+		//     east: 1600,
+		//     west: 1000,
+		//   },
+		// },
+		minZoom: 3,
+		maxZoom: 9
+		//mapTypeId: 'terrain'
+	};
+	const center = {
+		lat: -3.745,
+		lng: -38.523
+	};
 
-  return (
-    <Box bg='blue' h='100vh' w='100%'>
-      <LoadScript googleMapsApiKey={MAPKEY}>
-        <GoogleMap mapContainerStyle={mapContainerStyle} options={mapOptions} center={center} zoom={10}>
-          {
-            // ...Your map components
-          }
-        </GoogleMap>
-      </LoadScript>
-    </Box>
-  );
+	return (
+		<Box bg="blue" h="100vh" w="100%">
+			<LoadScript googleMapsApiKey={MAPKEY}>
+				<GoogleMap mapContainerStyle={mapContainerStyle} options={mapOptions} center={center} zoom={10}>
+					<MarkerF position={{ lat: 40.712776, lng: -74.005974 }} icon={'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'} />
+				</GoogleMap>
+			</LoadScript>
+		</Box>
+	);
 };

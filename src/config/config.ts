80095a9ef5b461de93cd4072f023664456d89e21
franziskafschulz <franziska.f.schulz@gmail.com>
2022-11-01 11:51:44 +0100

    socket config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,8 @@
-const gameServer = 'http://localhost:5000/';
+let gameServer = "http://localhost:5000/";
 
-// TODO: Add proper config for gameserver once deployed to heroku
-// if (process.env.NODE_ENV === "production") {
-// gameServer = "https://candi-server.herokuapp.com/";
-// console.log(process.env)
-//}
+if (process.env.NODE_ENV === "production") {
+  gameServer = "https://aftermath-server.onrender.com/";
+  console.log(process.env);
+}
 
 export { gameServer };
